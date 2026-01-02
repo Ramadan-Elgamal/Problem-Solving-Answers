e7c908723a1b9f25ed387660e79b05d1f46fd576
Ramadan-Elgamal <ramadanbotsher@gmail.com>
2026-01-02 19:21:13 +0200

    Update Summation 1 to N.cpp

--- Codeforces/Assuit Sheet/Summation 1 to N.cpp
+++ Codeforces/Assuit Sheet/Summation 1 to N.cpp
@@ -1,1 +1,11 @@
+#include <iostream>
 
+using namespace std;
+
+int main() {
+    long long n;
+    if(!(cin>>n)) return 0;
+    long long total = n * (n + 1) / 2;
+    cout<<total;
+    return 0;
+}
